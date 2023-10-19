a130a9f05265376bf41df36b23902c74e68a3402
Ronald <ronaldbresan@gmail.com>
2023-10-19 10:52:43 +0200

    Apellidos

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,7 @@
-public class Prueba{
-	Public static void main(String args[]) {
+public class Prueba {
+	
+	public static void main(String args[]) {
 		
 		System.out.println("Breton Sanchez");
-		
+	}
+}
