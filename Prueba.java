8be63301ff0b8cb931f4f0bb375ed635b1eb719d
Ronald <ronaldbresan@gmail.com>
2023-10-24 12:28:24 +0200

    Grupo favorito

--- Prueba.java
+++ Prueba.java
@@ -1,9 +1,9 @@
-public class Prueba {
+ public class Prueba {
 	
 	public static void main(String args[]) {
 		
-		System.out.println("Breton Sanchez");
 		System.out.println("21,2002");
 		System.out.println("hamburguesas");
+		System.out.println("Artic Monkeys");
 	}
 }
